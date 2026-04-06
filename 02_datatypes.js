2ce6c2044b0ae5bc6eccbaa32106d5d6ed04fffe
Vaibhav Nirwal <vaibhavnirwal047@gmail.com>
2026-04-06 22:38:07 +0530

    Add files via upload

---
+++ 02_datatypes.js
@@ -0,0 +1,32 @@
+ "use strict";  // treat all JS code as new version 
+
+//  alert("hello") // we are using nodejs , not browser
+
+console.log(3+3);  
+console.log("vaibhav");
+
+// code readibility should be high
+
+let name = "vaibhav"
+let age = 18
+let isloggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true /false
+// null => standalone value 
+// undefined => value not assigned
+// symbol => used for uniqueness
+
+
+//object 
+
+console.log(typeof "vaibhav"); // data type --> string
+
+console.log(typeof undefined); // datd type --> undefined 
+
+console.log(typeof null);  // data type --> object
+
+
+
