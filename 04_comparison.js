a26ff5a430e1bdd630ebdee01439d4298bbf68b8
Vaibhav Nirwal <vaibhavnirwal047@gmail.com>
2026-04-08 19:59:12 +0530

    Add files via upload

---
+++ 04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined > 0);
+
+
+// ( === ) --> check the equility of data type 
+console.log("2" === 2);
+
